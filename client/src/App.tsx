da46cf39eeed67bf9aa120e73d7f5fe05fe9f717
Josh McLain <joshmclain45@gmail.com>
2023-03-24 18:52:52 -0700

    👌 IMPROVE: removed repeated logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,8 +13,8 @@ type RadioCheckedT = {
 };
 type StateT = {
     questionList: QuestionT[];
-    currentQuestionIndex: number | null;
-    answerScoreHistory: Array<number>;
+    questionIndex: number | null;
+    answerHistory: Array<number>;
     canProceed: boolean;
     isChecked: RadioCheckedT;
     isCompleted: boolean;
@@ -24,14 +24,14 @@ type StateT = {
 const App: FC = () => {
     const [state, setState] = useState<StateT>({
         questionList: [...DATA],
-        currentQuestionIndex: null,
-        answerScoreHistory: [],
+        questionIndex: null,
+        answerHistory: [],
         canProceed: false,
         isChecked: { answer1: false, answer2: false, answer3: false, answer4: false },
         isCompleted: false,
         finalScore: 0,
     });
-    const { questionList, currentQuestionIndex, canProceed, isChecked, isCompleted, finalScore } = state;
+    const { questionList, questionIndex, canProceed, isChecked, isCompleted, finalScore } = state;
     useEffect(() => {
         console.log('🚀 ~ file: App.tsx:32 ~ state:', state);
     }, [state]);
@@ -51,41 +51,27 @@ const App: FC = () => {
             };
         });
     }, []);
-    const handleNext = (score: number) => {
-        setState((prevState) => {
-            const copyRadioState = { ...prevState.isChecked };
-            for (const key in copyRadioState) {
-                copyRadioState[key as keyof RadioCheckedT] = false;
-            }
-
-            return {
-                ...prevState,
-                currentQuestionIndex:
-                    prevState.currentQuestionIndex !== null &&
-                    prevState.currentQuestionIndex + 1 < prevState.questionList.length
-                        ? prevState.currentQuestionIndex + 1
-                        : prevState.currentQuestionIndex,
-                answerScoreHistory: prevState.answerScoreHistory.concat(score),
-                canProceed: false,
-                isChecked: copyRadioState,
-            };
-        });
+    const navigateIndex = (index: number | null, arrLength: number): number => {
+        if (index !== null) {
+            if (index + 1 < arrLength) return index + 1;
+            else if (index > 0) return index - 1;
+            else return index;
+        } else return 0;
     };
-    const handlePrevious = () => {
+    const handleNavigation = (scoreDirection?: number) => {
         setState((prevState) => {
-            const copyRadioState = { ...prevState.isChecked };
-            for (const key in copyRadioState) {
-                copyRadioState[key as keyof RadioCheckedT] = false;
+            const copyRadioChecked = { ...prevState.isChecked };
+            for (const key in copyRadioChecked) {
+                copyRadioChecked[key as keyof RadioCheckedT] = false;
             }
             return {
                 ...prevState,
-                currentQuestionIndex:
-                    prevState.currentQuestionIndex !== null && prevState.currentQuestionIndex > 0
-                        ? prevState.currentQuestionIndex - 1
-                        : prevState.currentQuestionIndex,
-                answerScoreHistory: prevState.answerScoreHistory.slice(0, -1),
+                questionIndex: navigateIndex(prevState.questionIndex, prevState.questionList.length),
+                answerHistory: scoreDirection
+                    ? prevState.answerHistory.concat(scoreDirection)
+                    : prevState.answerHistory.slice(0, -1),
                 canProceed: false,
-                isChecked: copyRadioState,
+                isChecked: copyRadioChecked,
             };
         });
     };
@@ -105,30 +91,29 @@ const App: FC = () => {
     };
     const handleSubmit = (event: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
         event.preventDefault();
-        const formControl = event.target as HTMLFormElement;
-        const controlEleCol = formControl.elements;
+        const formControls: HTMLFormControlsCollection = event.currentTarget.elements;
         let checkedScore = 0;
-        for (const ele in controlEleCol) {
-            const controlEle = controlEleCol[ele] as HTMLInputElement;
-            if (controlEle.type === 'radio' && controlEle.checked) checkedScore = parseInt(controlEle.value);
+        for (const E in formControls) {
+            const element = formControls[E] as HTMLInputElement;
+            if (element.type === 'radio' && element.checked) checkedScore = parseInt(element.value);
         }
-        const buttonClicked = event.nativeEvent.submitter as HTMLButtonElement;
-        switch (buttonClicked.value) {
+        const buttonEvent = event.nativeEvent.submitter as HTMLButtonElement;
+        switch (buttonEvent.value) {
             case 'start':
-                setState((prevState) => ({ ...prevState, currentQuestionIndex: 0 }));
+                setState((prevState) => ({ ...prevState, questionIndex: 0 }));
                 break;
             case 'next':
-                handleNext(checkedScore);
+                handleNavigation(checkedScore);
                 break;
             case 'previous':
-                handlePrevious();
+                handleNavigation();
                 break;
             case 'finish':
-                handleNext(checkedScore);
+                handleNavigation(checkedScore);
                 setState((prevState) => ({
                     ...prevState,
                     isCompleted: true,
-                    finalScore: prevState.answerScoreHistory.reduce((a, b) => a + b),
+                    finalScore: prevState.answerHistory.reduce((a, b) => a + b),
                 }));
                 break;
             case 'submit':
@@ -143,98 +128,86 @@ const App: FC = () => {
         else if (completed) return 'Final Results';
         else return 'Start intro prompting';
     };
+
+    const FormInputs =
+        questionIndex !== null &&
+        !isCompleted &&
+        questionList[questionIndex].response.map((obj: AnswerT, index: number) => {
+            const checked: boolean = isChecked[('answer' + (index + 1)) as keyof RadioCheckedT];
+            return (
+                <div key={uuid()} className="w-4/5 bg-white px-8 py-10 mb-2 rounded-2xl shadow-md">
+                    <input
+                        type="radio"
+                        id={`answer${index + 1}`}
+                        className="accent-slate-600 w-5 h-5 align-middle"
+                        name="answer"
+                        value={obj.score}
+                        checked={checked}
+                        onChange={handleChange}
+                    />
+                    <label className="px-2" htmlFor={`answer${index + 1}`}>
+                        {`${obj.score}---${obj.answer}`}
+                    </label>
+                </div>
+            );
+        });
+    const ResultsDisplay = isCompleted && (
+        <>
+            <h2 className="text-xl">Your score is {finalScore}</h2>
+            <div className="w-1/3 relative">
+                <AtSymbol size={20} className="absolute inset-y-0 left-1 align-middle opacity-40 h-full" />
+                <input type="email" className="input-default pl-6" placeholder="Email address" />
+            </div>
+        </>
+    );
+    const FormControlButtons =
+        questionIndex !== null && !isCompleted ? (
+            <>
+                <button
+                    type="submit"
+                    value={questionIndex >= questionList.length - 1 ? 'finish' : 'next'}
+                    className={`btn-default${!canProceed ? ' cursor-not-allowed' : ''}`}
+                    disabled={!canProceed}
+                >
+                    <span className="font-semibold">
+                        {questionIndex >= questionList.length - 1 ? 'Complete' : 'Next Question'}
+                    </span>
+                    {questionIndex >= questionList.length - 1 && (
+                        <ChevronRight className="w-5 h-5 inline align-middle" />
+                    )}
+                </button>
+                {questionIndex !== 0 && (
+                    <button type="submit" value="previous" className="btn-default">
+                        <ChevronLeft className="w-5 h-5 inline" />
+                        <span className="align-middle font-semibold">Previous Question</span>
+                    </button>
+                )}
+            </>
+        ) : (
+            <button type="submit" value={isCompleted ? 'submit' : 'start'} className="btn-default font-semibold">
+                {isCompleted ? 'Submit Email' : 'Get Started'}
+            </button>
+        );
     return (
         <div className="h-screen w-100">
-            <div className="h-full pt-32">
+            <div className="h-full pt-16">
                 <div className="flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold text-black">Assessment Questioner</h1>
                     <div className="min-h-96 w-2/4 bg-gray-200 mt-12 p-4 pb-8 rounded-lg shadow-xl">
                         <h2 className="text-center text-2xl mb-8 font-semibold">
                             {renderHeadingText(
-                                currentQuestionIndex !== null ? questionList[currentQuestionIndex].question : '',
-                                currentQuestionIndex,
+                                questionIndex !== null ? questionList[questionIndex].question : '',
+                                questionIndex,
                                 isCompleted,
                             )}
                         </h2>
                         <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center relative">
-                            {currentQuestionIndex !== null &&
-                                !isCompleted &&
-                                questionList[currentQuestionIndex].response.map((obj: AnswerT, index: number) => {
-                                    const checked: boolean = isChecked[('answer' + (index + 1)) as keyof RadioCheckedT];
-                                    return (
-                                        <div
-                                            key={uuid()}
-                                            className="w-4/5 bg-white px-8 py-10 mb-2 rounded-2xl shadow-md"
-                                        >
-                                            <input
-                                                type="radio"
-                                                id={`answer${index + 1}`}
-                                                className="accent-slate-600 w-5 h-5 align-middle"
-                                                name="answer"
-                                                value={obj.score}
-                                                checked={checked}
-                                                onChange={handleChange}
-                                            />
-                                            <label className="px-2" htmlFor={`answer${index + 1}`}>
-                                                {`${obj.score}---${obj.answer}`}
-                                            </label>
-                                        </div>
-                                    );
-                                })}
+                            {FormInputs}
+                            {ResultsDisplay}
 
-                            {isCompleted && (
-                                <>
-                                    <h2 className="text-xl">Your score is {finalScore}</h2>
-                                    <div className="w-1/3 relative">
-                                        <AtSymbol
-                                            size={20}
-                                            className="absolute inset-y-0 left-1 align-middle opacity-40 h-full"
-                                        />
-                                        <input
-                                            type="email"
-                                            className="input-default pl-6"
-                                            placeholder="Email address"
-                                        />
-                                    </div>
-                                </>
-                            )}
-                            <div className="flex justify-around flex-row-reverse w-1/2 mt-3">
-                                {currentQuestionIndex !== null && !isCompleted ? (
-                                    <>
-                                        <button
-                                            type="submit"
-                                            value={currentQuestionIndex >= questionList.length - 1 ? 'finish' : 'next'}
-                                            className={`btn-default${!canProceed ? ' cursor-not-allowed' : ''}`}
-                                            disabled={!canProceed}
-                                        >
-                                            <span className="font-semibold">
-                                                {currentQuestionIndex >= questionList.length - 1
-                                                    ? 'Complete'
-                                                    : 'Next Question'}
-                                            </span>
-                                            {currentQuestionIndex >= questionList.length - 1 && (
-                                                <ChevronRight className="w-5 h-5 inline align-middle" />
-                                            )}
-                                        </button>
-                                        {currentQuestionIndex !== 0 && (
-                                            <button type="submit" value="previous" className="btn-default">
-                                                <ChevronLeft className="w-5 h-5 inline" />
-                                                <span className="align-middle font-semibold">Previous Question</span>
-                                            </button>
-                                        )}
-                                    </>
-                                ) : (
-                                    <button
-                                        type="submit"
-                                        value={isCompleted ? 'submit' : 'start'}
-                                        className="btn-default font-semibold"
-                                    >
-                                        {isCompleted ? 'Submit Email' : 'Get Started'}
-                                    </button>
-                                )}
-                            </div>
-                            {currentQuestionIndex !== null && !isCompleted && (
-                                <span className="absolute right-2 bottom-2">{`Question ${currentQuestionIndex + 1} of ${
+                            <div className="flex justify-around flex-row-reverse w-1/2 mt-3">{FormControlButtons}</div>
+                            {questionIndex !== null && !isCompleted && (
+                                <span className="absolute right-2 bottom-2">{`Question ${questionIndex + 1} of ${
                                     questionList.length
                                 }`}</span>
                             )}
